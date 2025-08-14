5b9d2d89ec80c387358f32efef3b61d73cc24411
sunnyspain2012-hash <sunnyspain2012@gmail.com>
2025-08-14 19:48:30 +0500

    Update StationAdapter.java

--- app/src/main/java/com/virk/pakradiostations/StationAdapter.java
+++ app/src/main/java/com/virk/pakradiostations/StationAdapter.java
@@ -1,39 +1,53 @@
 package com.virk.pakradiostations;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
 
 public class StationAdapter extends RecyclerView.Adapter<StationAdapter.StationViewHolder> {
-    private List<RadioStation> stations;
 
-    public static class StationViewHolder extends RecyclerView.ViewHolder {
-        public TextView nameText;
-        public StationViewHolder(TextView v) {
-            super(v);
-            nameText = v;
-        }
-    }
+    private final List<RadioStation> stations;
+    private final StationClickListener clickListener;
 
-    public StationAdapter(List<RadioStation> stations) {
+    public StationAdapter(List<RadioStation> stations, StationClickListener clickListener) {
         this.stations = stations;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public StationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_station, parent, false);
+        return new StationViewHolder(view);
     }
 
     @Override
-    public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(android.R.layout.simple_list_item_1, parent, false);
-        return new StationViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(StationViewHolder holder, int position) {
-        holder.nameText.setText(stations.get(position).getName());
+    public void onBindViewHolder(@NonNull StationViewHolder holder, int position) {
+        RadioStation station = stations.get(position);
+        holder.nameView.setText(station.getName());
+        holder.itemView.setOnClickListener(v -> clickListener.onStationClick(position));
     }
 
     @Override
     public int getItemCount() {
         return stations.size();
+    }
+
+    static class StationViewHolder extends RecyclerView.ViewHolder {
+        TextView nameView;
+
+        public StationViewHolder(View itemView) {
+            super(itemView);
+            nameView = itemView.findViewById(R.id.stationName);
+        }
+    }
+
+    public interface StationClickListener {
+        void onStationClick(int position);
     }
 }
