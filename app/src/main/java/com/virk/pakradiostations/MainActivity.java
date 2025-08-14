e822e7c88fef2bc0fc8b12f02946cb381ee4d991
sunnyspain2012-hash <sunnyspain2012@gmail.com>
2025-08-14 19:17:39 +0500

    Update MainActivity.java

--- app/src/main/java/com/virk/pakradiostations/MainActivity.java
+++ app/src/main/java/com/virk/pakradiostations/MainActivity.java
@@ -1,1 +1,34 @@
+package com.virk.pakradiostations;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+
+public class MainActivity extends AppCompatActivity {
+    private ExoPlayer player;
+    private BarVisualizer visualizer;
+    private int currentStationIndex = 0;
+    private List<RadioStation> stations = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Initialization and player setup code here
+        // (Refer to previous full implementation)
+    }
+
+    // ... (Other methods like playStation(), togglePlayback() etc.)
+}
